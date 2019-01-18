cda93cbfa9d390ae8c42253c76f461a922e7232d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-18 08:31:53 -0700

    add to item icon presentation

--- app/models/item_presenter.rb
+++ app/models/item_presenter.rb
@@ -12,6 +12,14 @@ class ItemPresenter
       '<i class="fa fa-user" aria-hidden="true"></i>'.html_safe
     when "manga"
       '<i class="fas fa-book"></i>'.html_safe
+    when  "science"
+      '<i class="fas fa-microscope"></i>'.html_safe
+    when "web-development"
+      '<i class="fas fa-laptop"></i>'.html_safe
+    when "music"
+      '<i class="fa fa-music" aria-hidden="true"></i>'.html_safe
+    when "youtube-search"
+      '<i class="fab fa-youtube"></i>'.html_safe
     end
   end
 
