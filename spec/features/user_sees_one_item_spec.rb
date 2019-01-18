e202a6e9e6d560b82935796568ae00974d7e324d
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-17 21:07:23 -0700

    fix path

--- spec/features/user_sees_one_item_spec.rb
+++ spec/features/user_sees_one_item_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe "Item#show" do
   it "shows one item" do
     title_1 = "Star Wars"
-    Item.create(category: "film", name: title_1)
+    item = Item.create(category: "film", name: title_1)
     Item.create(category: "film", name: "Empire Strikes Back")
     Item.create(category: "film", name: "Return of the Jedi")
     visit '/items'
-    binding.pry
 
     click_link(title_1)
 
-    expect(current_path).to eq(item_path(Item.first))
+    expect(current_path).to eq(item_path(item))
 
   end
 end
