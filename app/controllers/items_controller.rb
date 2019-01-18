aa9dc1545351c4bda3082507320c55cfd26e31f1
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-17 22:54:56 -0700

    tweaks to show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,8 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    # cache this
+    @images = GoogleImageService.new(@item.name).img_array.first(8)
+    @videos = YoutubeService.new(@item.name).embed_links.uniq.first(8)
   end
 end
