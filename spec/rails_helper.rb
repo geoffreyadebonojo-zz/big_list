445360e9c6aa9bf14c99db254e974b1dda180d7a
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-17 21:05:23 -0700

    simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
+SimpleCov.start "rails"
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
