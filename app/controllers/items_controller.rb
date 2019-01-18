c0e09564bc92544ebcaf872b16b5f8cb066ca3e3
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-18 15:59:31 -0700

    make new and create methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,30 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     # cache this
-    @images = GoogleImageService.new(@item.name).img_array.first(8)
-    @videos = YoutubeService.new(@item.name).embed_links.uniq.first(8)
+    # @images = GoogleImageService.new(@item.name).img_array.first(8)
+    @videos = YoutubeService.new(@item.name).embed_links.uniq.first(16)
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to items_path
+  end
+
+  def new
+    h = Hash.new("")
+    icons = Item.all.map do |x|
+      ItemPresenter.new(x).category
+    end.uniq
+
+    @icons_array = icons.zip(Item.all.pluck(:category).uniq)
+    @icons_array.pop
+
+    @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category)
   end
 end
