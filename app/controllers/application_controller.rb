9eb156de17a4aa6270e3a99fff66846009c853c6
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-19 16:44:51 -0700

    add now playing helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  helper_method :random_documentary
+  helper_method :random_documentary, :now_playing, :other_songs
 
   def random_documentary
     topics = ["history", "nature", "space", "science", "biographies", "archaeology", "movies", "mystery"]
     @documentary ||= DocumentaryGetter.new(topics.sample).find_links.sample
     binding.pry
   end
+
+  def now_playing
+    @playing ||= Music.all.sample
+  end
+
 end
