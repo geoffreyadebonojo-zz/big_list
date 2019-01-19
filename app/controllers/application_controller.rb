84a03bcaea65f09363e8e7434bdad89e5ace450e
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-01-19 12:42:44 -0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :random_documentary
+
+  def random_documentary
+    topics = ["history", "nature", "space", "science", "biographies", "archaeology", "movies", "mystery"]
+    @documentary ||= DocumentaryGetter.new(topics.sample).find_links.sample
+    binding.pry
+  end
 end
