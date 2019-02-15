2303e6eec5cd8903c867a2e8f47d39dea9f7442a
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-15 08:12:28 -0700

    Add notes to schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :notes
   resources :videos
   get '/', to: 'welcome#index'
   resources :music
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_170213) do
+ActiveRecord::Schema.define(version: 2019_02_15_014209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2019_02_13_170213) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "videos", force: :cascade do |t|
