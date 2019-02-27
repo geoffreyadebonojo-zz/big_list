9027cffb4ae483a18fa1684eace5a98b6f6cd9d5
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-27 09:11:00 -0700

    Create seed file for music and vids

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Music.create(name: "Lofi Hip Hop Radio", url: "https://www.youtube.com/embed/hHW1oY26kxQ")
+Music.create(name: "Mozart", url: "https://www.youtube.com/embed/wzeRSsNHtsQ")
+Music.create(name: "Zelda Music", url: "https://www.youtube.com/embed/GbuJURjHm9M")
+Music.create(name: "Zelda Morning", url: "https://www.youtube.com/embed/fzpCAT4jiRE")
+Music.create(name: "Zelda Medley", url: "https://www.youtube.com/embed/NfHT7brehoc")
+
+Video.create(uri: "https://youtube.com/embed/5pNz_Dyf9_c")
+Video.create(uri: "https://youtube.com/embed/C4t6qfHZ6Tw")
+Video.create(uri: "https://youtube.com/embed/RVA4HoEE_q8")
+Video.create(uri: "https://youtube.com/embed/K3FMuLT_3Ik")
+Video.create(uri: "https://youtube.com/embed/CkFktv0p3pw")
+Video.create(uri: "https://youtube.com/embed/sEpRzyPRH0s")
